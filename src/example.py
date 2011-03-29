4ab54b006ab4afaf329db33c4f7cf622d0c6a153
Hervé Cauwelier <herve@itaapy.com>
2011-03-29 15:31:37 +0200

    remove "of_" from stats functions

--- src/example.py
+++ src/example.py
@@ -5,7 +5,7 @@ from random import randrange
 
 #import lpod_vle
 from syntax import OF
-from stats import of_average, of_min, of_max
+from stats import AVERAGE, MIN, MAX
 
 #We create a spreadsheet just for the example
 doc = odf_new_document('spreadsheet')
@@ -20,28 +20,25 @@ for c in range(5):
         table.set_value((c, l), v)
 
 #Then, we write some statistics about the data in a new sheet
-table = odf_create_table('Stats', width=3, height=2)
+table = odf_create_table(u'Stats', width=3, height=2)
 body.append(table)
-table.set_value((0,0), "Average")
-table.set_value((1,0), "Maximum")
-table.set_value((2,0), "Minimum")
+table.set_value((0,0), u"Average")
+table.set_value((1,0), u"Maximum")
+table.set_value((2,0), u"Minimum")
 
 #average
-formula=OF(of_average(u'Data.A1:E5'))
-cell = odf_create_cell()
-cell.set_formula(formula)
+formula = OF(AVERAGE(u'Data.A1:E5'))
+cell = odf_create_cell(formula=formula)
 table.set_cell((0,1), cell)
 
 #maximum
-formula=OF(of_max(u'Data.A1:E5'))
-cell = odf_create_cell()
-cell.set_formula(formula)
+formula = OF(MAX(u'Data.A1:E5'))
+cell = odf_create_cell(formula=formula)
 table.set_cell((1,1), cell)
 
 #minimum
-formula=OF(of_min('Data.A1:E5'))
-cell = odf_create_cell()
-cell.set_formula(formula)
+formula = OF(MIN('Data.A1:E5'))
+cell = odf_create_cell(formula=formula)
 table.set_cell((2,1), cell)
 
 doc.save("out.ods")
--- src/test_stats.py
+++ src/test_stats.py
@@ -1,33 +1,32 @@
 import unittest
 
-from objects import RangeReference
-from stats import of_max, of_min, of_average, of_sumproduct
+from stats import MAX, MIN, AVERAGE, SUMPRODUCT
 
 
 class TestOpenFormulaFunctions(unittest.TestCase):
 
 #Statistics functions
-    def test_of_max(self):
-        self.assertEqual(of_max("A1:B2"), "MAX([.A1:.B2])")
-	self.assertEqual(of_max("A1:B2","C3:D4"),"MAX([.A1:.B2] ; [.C3:.D4])")
-        self.assertRaises(TypeError, of_max, 2)
+    def test_max(self):
+        self.assertEqual(MAX("A1:B2"), "MAX([.A1:.B2])")
+	self.assertEqual(MAX("A1:B2","C3:D4"),"MAX([.A1:.B2] ; [.C3:.D4])")
+        self.assertRaises(TypeError, MAX, 2)
 
-    def test_of_min(self):
-        self.assertEqual(of_min("A1:B2"), "MIN([.A1:.B2])")
-	self.assertEqual(of_min("A1:B2","C3:D4"),"MIN([.A1:.B2] ; [.C3:.D4])")
-        self.assertRaises(TypeError, of_min, 2)
+    def test_min(self):
+        self.assertEqual(MIN("A1:B2"), "MIN([.A1:.B2])")
+	self.assertEqual(MIN("A1:B2","C3:D4"),"MIN([.A1:.B2] ; [.C3:.D4])")
+        self.assertRaises(TypeError, MIN, 2)
 
-    def test_of_average(self):
-	self.assertEqual(of_average("A1:B2"),"AVERAGE([.A1:.B2])")
-	self.assertEqual(of_average("A1:B2","C3:D4"), 
+    def test_average(self):
+	self.assertEqual(AVERAGE("A1:B2"),"AVERAGE([.A1:.B2])")
+	self.assertEqual(AVERAGE("A1:B2","C3:D4"), 
                                              "AVERAGE([.A1:.B2] ; [.C3:.D4])")
-        self.assertRaises(TypeError, of_average, 2)
+        self.assertRaises(TypeError, AVERAGE, 2)
 
-    def test_of_sumproduct(self):
-	self.assertEqual(of_sumproduct("A1:B2"),"SUMPRODUCT([.A1:.B2])")
-	self.assertEqual(of_sumproduct("A1:B2","C3:D4"),
+    def test_sumproduct(self):
+	self.assertEqual(SUMPRODUCT("A1:B2"),"SUMPRODUCT([.A1:.B2])")
+	self.assertEqual(SUMPRODUCT("A1:B2","C3:D4"),
                                           "SUMPRODUCT([.A1:.B2] ; [.C3:.D4])")
-        self.assertRaises(TypeError, of_sumproduct, 2)
+        self.assertRaises(TypeError, SUMPRODUCT, 2)
 
 if __name__ == '__main__':
     unittest.main()
